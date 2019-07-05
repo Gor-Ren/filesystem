6634eb5bcd17b5fdb13378497ec61d4f5e1a7992
Gor-Ren <gor.rennie@gmail.com>
2019-07-05 23:57:10 +0100

    Migrate project to sbt, add scalatest

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "filesystem"
+
+version := "0.1"
+
+scalaVersion := "2.12.8"
+
+resolvers in ThisBuild += "Artima Maven Repository" at "http://repo.artima.com/releases"
+
+libraryDependencies += "org.scalactic" %% "scalactic" % "3.0.8"
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.8" % "test"
---
+++ src/test/scala/gor/files/DirectoryTest.scala
@@ -0,0 +1,15 @@
+package gor.files
+
+import org.scalatest.{FlatSpec, Matchers}
+
+
+class DirectoryTest extends FlatSpec with Matchers {
+
+  behavior of "empty Directory factory method"
+
+  it should "have no contents" in {
+    val emptyDir = Directory.empty("/", "test")
+    emptyDir.contents shouldBe empty
+  }
+
+}
