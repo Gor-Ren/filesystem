198687f3975087a84eed004b86f9f853e2512a4b
Gor-Ren <gor.rennie@gmail.com>
2019-07-10 19:03:20 +0100

    Implement isLegalDirName()

--- src/main/scala/gor/commands/Mkdir.scala
+++ src/main/scala/gor/commands/Mkdir.scala
@@ -1,24 +1,30 @@
 package gor.commands
+
+import java.util.regex.Pattern
+
 import gor.files.Directory
 import gor.filesystem.State
 
 class Mkdir(val dirName: String) extends Command {
+  /** The legal format of a directory name; alphanumeric, dash or underscore */
+  val legalDirPattern: Pattern = Pattern.compile("^[a-zA-Z0-9\-_]*$")
+
 
   override def apply(state: State): State = {
     val wd = state.wd
     if (wd.hasEntry(dirName)) {
-      state.setMessage("Entry " + dirName + "already exists.")
+      state.setMessage(s"Entry $dirName already exists.")
     } else if (dirName.contains(Directory.SEPARATOR)) {
-      state.setMessage(dirName + " must not contain separators.")
+      state.setMessage(s"$dirName must not contain separators.")
     } else if (!isLegalDirName(dirName)) {
-      state.setMessage(dirName + ": illegal directory name!")
+      state.setMessage(s"$dirName: illegal directory name.")
     } else {
       doMkdir(state, dirName)
     }
   }
 
-  def isLegalDirName(str: String): Boolean = {
-    ???
+  private def isLegalDirName(proposedName: String): Boolean = {
+    legalDirPattern.matcher(proposedName).matches()
   }
 
   def doMkdir(state: State, dirName: String): State = {
