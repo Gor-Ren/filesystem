ff243b77157f921d099f96ce1693e51dde7e8383
Gor-Ren <gor.rennie@gmail.com>
2019-07-13 16:21:10 +0100

    Make Filesystem testable and begin integration tests

--- src/it/scala/gor/filesystem/FilesystemIntTest.scala
+++ src/it/scala/gor/filesystem/FilesystemIntTest.scala
@@ -1,28 +1,43 @@
 package gor.filesystem
 
-import java.io.{ByteArrayInputStream, PipedOutputStream}
-import java.util.concurrent.{Executor, Executors}
+import java.io._
+import java.nio.charset.StandardCharsets
+import java.util.Scanner
 
-import org.scalatest.{BeforeAndAfter, FeatureSpec, GivenWhenThen}
+import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
 
-import scala.concurrent.{ExecutionContext, Future}
+class FilesystemIntTest extends FlatSpec with Matchers with BeforeAndAfter {
 
-class FilesystemIntTest
-    extends FeatureSpec
-    with GivenWhenThen
-    with BeforeAndAfter {
+  var userInput: Writer = _
+  var userOutput: Scanner = _
+  var filesystemThread: Thread = _
 
-  feature("The user is given a suitable response on invalid command") {
-    scenario("User enters an empty command") {
-      Given("the user is on the command line")
-//      val userInput = new PipedOutputStream()
-//      val filesystemThread = ExecutionContext.fromExecutorService(
-//        Executors.newSingleThreadExecutor()
-//      )
-//      Future(Filesystem.run(userInput))(filesystemThread)
+  before {
+    val filesystemInput = new PipedInputStream()
+    val filesystemOutput = new PipedOutputStream()
 
-      When("they hit ENTER without having entered a command")
-      Then("a blank output is given and the state is unchanged")
-    }
+    userInput = new OutputStreamWriter(new PipedOutputStream(filesystemInput))
+    userOutput = new Scanner(
+      new PipedInputStream(filesystemOutput),
+      StandardCharsets.UTF_8.name()
+    ).useDelimiter("\\A") // \A is the start of any string
+
+    filesystemThread =
+      new Thread(() => Filesystem(filesystemInput, filesystemOutput).run())
+    filesystemThread.start()
+  }
+
+  after {
+    userInput.close()
+    userOutput.close()
+    filesystemThread.stop()
+  }
+
+  behavior of "the filesystem program"
+
+  ignore should "return an empty output when user makes empty input" in {
+    userInput.write("\n")
+    val output: String = userOutput.next()
+    output shouldBe "\n$ "
   }
 }
--- src/main/scala/gor/filesystem/Filesystem.scala
+++ src/main/scala/gor/filesystem/Filesystem.scala
@@ -1,30 +1,37 @@
 package gor.filesystem
 
-import java.io.InputStream
+import java.io.{InputStream, OutputStream}
 import java.util.Scanner
 
 import gor.commands.Command
 import gor.files.Directory
+import gor.filesystem.Filesystem.state
 
-/**
-  * The entry point and main control loop of the filesystem application.
-  */
+/** The entry point and main control loop of the filesystem application. */
+class Filesystem(input: InputStream, output: OutputStream) {
+
+  def run(): Unit = {
+    val scanner = new Scanner(input)
+
+    Console.withOut(output) {
+      while (true) {
+        state.show()
+        val input = scanner.nextLine()
+        state = Command.from(input).apply(state)
+      }
+    }
+  }
+}
+
 object Filesystem {
 
   val root: Directory = Directory.ROOT
   var state: State = State(root, root)
 
-  def run(input: InputStream): Unit = {
-    val scanner = new Scanner(input)
-
-    while (true) {
-      state.show()
-      val input = scanner.nextLine()
-      state = Command.from(input).apply(state)
-    }
-  }
+  def apply(in: InputStream, out: OutputStream): Filesystem =
+    new Filesystem(in, out)
 
   def main(args: Array[String]): Unit = {
-    run(System.in)
+    Filesystem(System.in, System.out).run()
   }
 }
