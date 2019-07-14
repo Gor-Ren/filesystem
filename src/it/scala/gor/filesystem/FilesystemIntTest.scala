9663db3f77bf572cd5f3f0c2cdc24d4fd0d53bd1
Gordon <gor.rennie@gmail.com>
2019-07-14 14:41:37 +0100

    Implement integration tests

--- src/it/scala/gor/filesystem/FilesystemIntTest.scala
+++ src/it/scala/gor/filesystem/FilesystemIntTest.scala
@@ -1,5 +1,62 @@
 package gor.filesystem
 
+import gor.commands.Command
+import gor.mocks.MockView
 import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
 
-class FilesystemIntTest extends FlatSpec with Matchers with BeforeAndAfter {}
+class FilesystemIntTest extends FlatSpec with Matchers with BeforeAndAfter {
+
+  def mockViewWithInputs(userInputs: Seq[String]): (MockView, Filesystem) = {
+    val mockView = MockView(userInputs)
+    (mockView, new Filesystem(mockView))
+  }
+
+  def mockViewWithInput(userInput: String): (MockView, Filesystem) = {
+    mockViewWithInputs(Seq(userInput))
+  }
+
+  behavior of "the filesystem program"
+
+  it should "output an empty string on start" in {
+    val (view, filesystem) = mockViewWithInputs(Seq.empty)
+    filesystem.run()
+    view.getOutputs should contain theSameElementsInOrderAs Seq("")
+  }
+
+  it should "complain about an unrecognised command" in {
+    val (view, filesystem) = mockViewWithInput("bad_command")
+    filesystem.run()
+    view.getOutputs should contain theSameElementsInOrderAs Seq(
+      "",
+      "Command not found."
+    )
+  }
+
+  it should "complain about too few args" in {
+    val (view, filesystem) = mockViewWithInput(Command.MKDIR)
+    filesystem.run()
+    view.getOutputs should contain theSameElementsInOrderAs Seq(
+      "",
+      "mkdir: too few arguments."
+    )
+  }
+
+  it should "complain about too many args" in {
+    val (view, filesystem) = mockViewWithInput(s"${Command.MKDIR} arg1 arg2")
+    filesystem.run()
+    view.getOutputs should contain theSameElementsInOrderAs Seq(
+      "",
+      "mkdir: too many arguments."
+    )
+  }
+
+  it should "create and navigate to a new directory" in {
+    val dirName = "myTestDir"
+    val (view, filesystem) = mockViewWithInput(s"${Command.MKDIR} $dirName")
+    filesystem.run()
+    view.getOutputs should contain theSameElementsInOrderAs Seq(
+      "",
+      "mkdir: too many arguments."
+    )
+  }
+}
--- src/it/scala/gor/mocks/MockViewTest.scala
+++ src/it/scala/gor/mocks/MockViewTest.scala
@@ -1,14 +1,41 @@
 package gor.mocks
 
 import gor.commands.Command
+import gor.files.Directory
+import gor.filesystem.State
 import org.scalatest.{FlatSpec, Matchers}
 
 import scala.collection.mutable.ListBuffer
 
 /** Unit tests ensuring correct behaviour of the mock view. */
 class MockViewTest extends FlatSpec with Matchers {
+  val testState = State(Directory.empty("/", "/"), Directory.empty("/", "/"))
 
-  behavior of "empty initialised mock view"
+  behavior of "any mock view"
+
+  it should "record outputs when updated" in {
+    val view = MockView(Seq.empty)
+    val message = "test-message"
+    view.update(testState.setMessage(message))
+    view.getOutputs should contain theSameElementsInOrderAs Seq(message)
+  }
+
+  it should "record outputs in the order of updates" in {
+    val view = MockView(Seq.empty)
+    val message1 = "test-message-one"
+    val message2 = "test-message-two"
+    view.update(testState.setMessage(message1))
+    view.update(testState.setMessage(message2))
+    view.getOutputs should contain theSameElementsInOrderAs Seq(message1,
+                                                                message2)
+  }
+
+  it should "show empty outputs when never updated" in {
+    val view = MockView(Seq.empty)
+    view.getOutputs shouldBe empty
+  }
+
+  behavior of "mock view (when initialised with no commands)"
 
   it should "show it contains a command" in {
     val view = MockView(Seq.empty)
@@ -21,6 +48,14 @@ class MockViewTest extends FlatSpec with Matchers {
     view.hasCommands shouldBe false
   }
 
+  it should "complain when reading input when empty" in {
+    intercept[NoSuchElementException] {
+      val view = MockView(Seq.empty)
+      view.readInput() // reads 'quit' command
+      view.readInput() // should throw
+    }
+  }
+
   it should "return one 'quit' command" in {
     val view = MockView(Seq.empty)
     var inputs = new ListBuffer[String]
@@ -28,7 +63,7 @@ class MockViewTest extends FlatSpec with Matchers {
     inputs.result() should contain theSameElementsInOrderAs Seq(Command.QUIT)
   }
 
-  behavior of "single command initialised mock view"
+  behavior of "mock view (when initialised with a single command)"
 
   it should "show it contains a command" in {
     val view = MockView(Seq("test"))
@@ -45,5 +80,24 @@ class MockViewTest extends FlatSpec with Matchers {
     view.readInput()
     view.readInput()
     view.hasCommands shouldBe false
+  }
+
+  it should "return the input command, followed by `quit`" in {
+    val command = "test"
+    val view = MockView(Seq(command))
+    view.readInput() shouldBe command
+    view.readInput() shouldBe Command.QUIT
+  }
+
+  behavior of "mock view (when initialised with multiple commands)"
+
+  it should "return the input commands in order, followed by `quit`" in {
+    val commands = Seq("1", "2", "3")
+    val view = MockView(commands)
+
+    val expectedOutput = commands ++ Seq(Command.QUIT)
+    expectedOutput.foreach(
+      cmd => view.readInput() shouldBe cmd
+    )
   }
 }
